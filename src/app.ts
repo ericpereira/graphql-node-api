16662735832b299f57132bb6c00b69912c5c1169
Eric Pereira <eric.pereira@hotmail.com>
2021-10-26 13:37:30 -0300

    add middleware for access db in context

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
-import * as express from 'express'
-import * as graphqlHTTP from 'express-graphql'
-import schema from './graphql/schema'
+import * as express from 'express';
+import * as graphqlHTTP from 'express-graphql';
+import schema from './graphql/schema';
+import db from './models';
 
 class App{
-    public express: express.Application
+    public express: express.Application;
 
     constructor(){
-        this.express = express()
-        this.middleware()
+        this.express = express();
+        this.middleware();
     }
 
     private middleware(): void{
-        this.express.use('/graphql', graphqlHTTP({
-            schema: schema,
-            graphiql: process.env.NODE_ENV === 'development'
-        }))
+        this.express.use('/graphql',
+
+            //adiciona o db ao contexto para não precisar ficar importando toda hora
+            (req, res, next) => {
+                req['context'] = {};
+                req['context'].db = db;
+                next();
+            },
+
+            //middleware que criar o servidor graphql para trabalhar com requisições http
+            graphqlHTTP((req) => ({ 
+                schema: schema,
+                graphiql: process.env.NODE_ENV === 'development',
+                context: req['context']
+            }))
+        );
+
     }
 }
 
-export default new App().express
+export default new App().express;
